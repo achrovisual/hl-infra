d05feed73685f8a2c831f61df012f6cdeda117a3
Eugenio Pastoral <pastoraleugenio@gmail.com>
2025-04-30 19:07:45 +0800

    Update formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     infisical = {
-      source = "Infisical/infisical"
+      source  = "Infisical/infisical"
       version = "0.15.9"
     }
 
     proxmox = {
-      source = "Telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "3.0.1-rc8"
     }
   }

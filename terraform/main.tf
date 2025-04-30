f19923c697ddcc66933703a06380f8625e8ee22b
Eugenio Pastoral <pastoraleugenio@gmail.com>
2025-04-30 18:40:44 +0800

    Initialize Infisical provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    infisical = {
+      source = "Infisical/infisical"
+      version = "0.15.9"
+    }
+
     proxmox = {
       source = "Telmate/proxmox"
       version = "3.0.1-rc8"
